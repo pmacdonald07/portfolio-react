d0ff33360b658b9f7e1669f8e7d93e8232e2a45f
Patrick M <patrickmacdonald07@gmail.com>
2022-12-20 13:14:23 -0500

    removed broken anchor tag

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,11 +3,7 @@ import Navigation from "../Navigation";
 const Header = ({ currentPage, handlePageChange }) => {
   return (
     <header className="d-md-flex d-block flex-wrap justify-content-between px-1 m-3">
-      <h2 className="text-center">
-        <a href="/" className="text-decoration-none text-reset">
-          Patrick MacDonald
-        </a>
-      </h2>
+      <h2 className="text-center">Patrick MacDonald</h2>
       <Navigation
         currentPage={currentPage}
         handlePageChange={handlePageChange}
